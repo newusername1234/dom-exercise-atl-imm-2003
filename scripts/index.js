d1df21cd08cb851a8b9db207721a1cd39a183ac7
Wes <wesleyayer92@gmail.com>
2020-04-22 11:17:20 -0400

    creates nav links

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,21 @@
 
-
 const addresses = [
     "https://google.com",
     "https://bing.com",
     "https://duckduckgo.com"
 ];
 
+const navLinks = document.createElement('ul');
+const navContainer = document.querySelector('.js-link-container');
+
+addresses.forEach(address => {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.setAttribute('href', address);
+    link.setAttribute('target', '_blank');
+    link.textContent = address;
+    listItem.appendChild(link);
+    navLinks.appendChild(listItem);
+});
+
+navContainer.appendChild(navLinks);
